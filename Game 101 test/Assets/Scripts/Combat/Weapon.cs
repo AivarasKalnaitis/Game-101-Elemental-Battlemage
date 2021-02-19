794795ef849dff82e248f5e4f35d1000f1eb33c5
Vilhelmasss <vilhelmasss@gmail.com>
2021-02-19 22:52:49 +0200

    Starting to work on fire

--- Game 101 test/Assets/Scripts/Combat/Weapon.cs
+++ Game 101 test/Assets/Scripts/Combat/Weapon.cs
@@ -7,11 +7,18 @@ public class Weapon : MonoBehaviour
 
     [SerializeField] private float offset;
 
+    public GameObject projectile;
+    public Transform shotPoint;
     void Update()
     {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
 
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(projectile, shotPoint.position, transform.rotation);
+        }
     }
 }
+    
