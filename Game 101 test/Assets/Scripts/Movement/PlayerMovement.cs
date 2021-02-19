b34090d2b2856ba1cfff706df52362e2dd824b72
Aivaras <aivariuxxxl@gmail.com>
2021-02-19 23:50:42 +0300

    fixed jump/fall gravity

--- Game 101 test/Assets/Scripts/Movement/PlayerMovement.cs
+++ Game 101 test/Assets/Scripts/Movement/PlayerMovement.cs
@@ -9,9 +9,21 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
 
     public float runSpeed = 40f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    Rigidbody2D rb;
 
     bool jump = false;
     bool crouch = false;
+
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,6 +33,16 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
 
         if (Input.GetButtonDown("Crouch"))
