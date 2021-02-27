4edeee68e0bae8e4092e516fd0dc9805b9e5807f
Aivaras <aivariuxxxl@gmail.com>
2021-02-27 20:26:08 +0200

    New red gems spawning

--- Game 101 test/Assets/Scripts/Items/TakeRedGem.cs
+++ Game 101 test/Assets/Scripts/Items/TakeRedGem.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class TakeRedGem : MonoBehaviour
 {
     public GameObject TextCanvas;
+    private EdgeCollider2D edgeCollider;
+    private TakeRedGem script;    
 
     void Awake()
     {
@@ -15,7 +17,7 @@ public class TakeRedGem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Instantiate(redGemPrefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -29,12 +31,26 @@ public class TakeRedGem : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             TextCanvas.GetComponent<RedGemsUI>().RedGemsPlus();
-
             Destroy(gameObject);
-
-
+            Debug.Log("Player touched me !");
+            spawnRedGem();
+            
         }
+    }
 
+    void spawnRedGem()
+    {
+        float spawnY = Random.Range
+                (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+        float spawnX = Random.Range
+            (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+        Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+
+        GameObject newRedGem = Instantiate(gameObject, spawnPosition, Quaternion.identity);
+        edgeCollider = newRedGem.GetComponent<EdgeCollider2D>();
+        edgeCollider.enabled = true;
+        script = newRedGem.GetComponent<TakeRedGem>();
+        script.enabled = true;
     }
 
 
