8992ce1e456f57395173c1ad3a2b62608fe6281c
Vilhelmasss <vilhelmasss@gmail.com>
2021-05-24 00:43:01 +0300

    kekw

---
+++ Game 101 test/Assets/Scripts/GUI/Inventory.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private bool inventoryEnabled = false;
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+
+    private GameObject[] Slot;
+
+    public GameObject slotHolder;
+
+    void Start()
+    {
+        allSlots = 26;
+
+        Slot = new GameObject[allSlots];
+        for (int i = 0; i < allSlots; i++)
+        {
+            Slot[i] = slotHolder.transform.GetChild(i).gameObject;
+
+            if (Slot[i].GetComponent<Slot>().item == null)
+            {
+                Slot[i].GetComponent<Slot>().empty = true;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if (inventoryEnabled)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Item")
+        {
+            GameObject itemPickedUp = other.gameObject;
+            InventoryItem item = itemPickedUp.GetComponent<InventoryItem>();
+
+            AddItem(itemPickedUp, item.ID, item.type, item.description, item.icon);
+//            AddItem()
+        }
+    }
+
+    void AddItem(GameObject itemObject, int itemID, string itemType, string itemDescription, Texture2D itemIcon)
+    {
+        for (int i = 0; i < allSlots; i++)
+        {
+            if (Slot[i].GetComponent<Slot>().empty)
+            {
+                itemObject.GetComponent<InventoryItem>().pickedUp = true;
+
+                Slot[i].GetComponent<Slot>().item = itemObject;        
+                Slot[i].GetComponent<Slot>().icon = itemIcon;
+                Slot[i].GetComponent<Slot>().type = itemType;
+                Slot[i].GetComponent<Slot>().ID = itemID;
+                Slot[i].GetComponent<Slot>().description = itemDescription;
+
+                itemObject.transform.parent = Slot[i].transform;
+                itemObject.SetActive(false);
+
+                Slot[i].GetComponent<Slot>().empty = false;
+
+            }
+        }
+    }
+}
---
+++ Game 101 test/Assets/Scripts/Items/InventoryItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public int ID;
+    public string type;
+    public string description;
+    public Texture2D icon;
+    public bool pickedUp;
+
+    void Start()
+    {
+            
+    }
+
+    void Update()
+    {
+        
+    }
+}
